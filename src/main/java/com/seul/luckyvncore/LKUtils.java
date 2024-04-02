3b880c65ce36168f8a828f92378bcda2261300c1
Seul <seulgaming174@gmail.com>
2024-04-02 12:29:30 +0700

    Add to first API and up to ver 1.18.2

---
+++ src/main/java/com/seul/luckyvncore/LKUtils.java
@@ -0,0 +1,36 @@
+package com.seul.luckyvncore;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LKUtils {
+
+    private static final Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
+
+    // ALLOW HEX COLOR
+    public static String color(String message) {
+        Matcher matcher = pattern.matcher(message);
+
+        while (matcher.find()) {
+            String color = message.substring(matcher.start(), matcher.end());
+            message = message.replace(color, "" + net.md_5.bungee.api.ChatColor.of(color));
+            matcher = pattern.matcher(message);
+        }
+
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+
+
+    // send message color
+    public static void sendMessageList(List<String> message, Player player) {
+        for (String s : message) {
+            player.sendMessage(color(s));
+        }
+    }
+
+
+}
