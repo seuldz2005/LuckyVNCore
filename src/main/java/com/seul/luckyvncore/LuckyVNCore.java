90be3d100ecfdf46d3167cc51b03762530b170d8
Seul <seulgaming174@gmail.com>
2024-04-02 12:22:06 +0700

    Initial commit

---
+++ src/main/java/com/seul/luckyvncore/LuckyVNCore.java
@@ -0,0 +1,17 @@
+package com.seul.luckyvncore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LuckyVNCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
