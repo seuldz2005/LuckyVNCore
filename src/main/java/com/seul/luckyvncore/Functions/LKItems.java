894d11b6cb4d955b63afc4a6855b94d33a5865f0
Seul <seulgaming174@gmail.com>
2024-11-27 11:14:47 +0700

    add LKItems function to create item so easy

---
+++ src/main/java/com/seul/luckyvncore/Functions/LKItems.java
@@ -0,0 +1,89 @@
+package com.seul.luckyvncore.Functions;
+
+import com.cryptomorin.xseries.XMaterial;
+import com.seul.luckyvncore.LKUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.profile.PlayerProfile;
+import org.bukkit.profile.PlayerTextures;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class LKItems {
+
+
+    private ItemStack item;
+    private List<String> lores;
+
+    public LKItems(String material, List<String> lores) {
+
+        item = material.equals("headbase") ? getHeadBase(material) : XMaterial.valueOf(material).parseItem();
+
+        lores = (lores == null) ? new ArrayList<>() : lores;
+        setLores(lores);
+    }
+
+    public ItemStack getItem() {
+        return item;
+    }
+
+    public List<String> getLores() {
+        return lores;
+    }
+
+    public void setLores(List<String> lores) {
+        ItemMeta meta = item.getItemMeta();
+        meta.setLore(lores.stream().map(s -> LKUtils.color(s)).collect(Collectors.toList()));
+        item.setItemMeta(meta);
+    }
+
+    public void replaceLores(String oldchar, String newchar) {
+        setLores(lores.stream().map(s -> s.replace(oldchar, newchar)).collect(Collectors.toList()));
+
+    }
+
+    private ItemStack getHeadBase(String material) {
+        URL url;
+        try {
+            url = getUrlFromBase64(material.replace("headbase-", ""));
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        PlayerProfile profile = getProfile(url.toString());
+        ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta meta = (SkullMeta) head.getItemMeta();
+        meta.setOwnerProfile(profile);
+        head.setItemMeta(meta);
+        return head;
+    }
+
+    private URL getUrlFromBase64(String base64) throws MalformedURLException {
+        String decoded = new String(Base64.getDecoder().decode(base64));
+        return new URL(decoded.substring("{\"textures\":{\"SKIN\":{\"url\":\"".length(), decoded.length() - "\"}}}".length()));
+    }
+
+    private final UUID RANDOM_UUID = UUID.fromString("92864445-51c5-4c3b-9039-517c9927d1b4");
+    private PlayerProfile getProfile(String url) {
+        PlayerProfile profile = Bukkit.createPlayerProfile(RANDOM_UUID);
+        PlayerTextures textures = profile.getTextures();
+        URL urlObject;
+        try {
+            urlObject = new URL(url);
+        } catch (MalformedURLException exception) {
+            throw new RuntimeException("Invalid URL", exception);
+        }
+        textures.setSkin(urlObject);
+        profile.setTextures(textures);
+        return profile;
+    }
+
+}
--- src/main/java/com/seul/luckyvncore/LKUtils.java
+++ src/main/java/com/seul/luckyvncore/LKUtils.java
@@ -1,182 +1,176 @@
-    package com.seul.luckyvncore;
+package com.seul.luckyvncore;
 
-    import me.clip.placeholderapi.PlaceholderAPI;
-    import org.bukkit.Bukkit;
-    import org.bukkit.ChatColor;
-    import org.bukkit.Location;
-    import org.bukkit.World;
-    import org.bukkit.block.Block;
-    import org.bukkit.block.BlockFace;
-    import org.bukkit.command.CommandSender;
-    import org.bukkit.command.ConsoleCommandSender;
-    import org.bukkit.entity.Player;
-    import org.bukkit.inventory.Inventory;
-    import org.bukkit.inventory.ItemStack;
-    import org.bukkit.util.Vector;
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 
-    import java.util.ArrayList;
-    import java.util.Iterator;
-    import java.util.List;
-    import java.util.regex.Matcher;
-    import java.util.regex.Pattern;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    public class LKUtils {
+public class LKUtils {
 
-        private static final Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
+    private static final Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
 
-        // ALLOW HEX COLOR
-        public static String color(String message) {
-            Matcher matcher = pattern.matcher(message);
+    // ALLOW HEX COLOR
+    public static String color(String message) {
+        Matcher matcher = pattern.matcher(message);
 
-            while (matcher.find()) {
-                String color = message.substring(matcher.start(), matcher.end());
-                message = message.replace(color, "" + net.md_5.bungee.api.ChatColor.of(color));
-                matcher = pattern.matcher(message);
-            }
-
-            return ChatColor.translateAlternateColorCodes('&', message);
+        while (matcher.find()) {
+            String color = message.substring(matcher.start(), matcher.end());
+            message = message.replace(color, "" + net.md_5.bungee.api.ChatColor.of(color));
+            matcher = pattern.matcher(message);
         }
 
-        public static void sendMessage(CommandSender player, String message) {
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
 
+    public static void sendMessage(CommandSender player, String message) {
+
+
+        if (player instanceof Player) {
+            if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+                player.sendMessage(color(PlaceholderAPI.setPlaceholders(((Player) player), message)));
+            }
+        } else player.sendMessage(color(message));
+    }
+
+    public static void sendMessage(CommandSender player, List<String> message) {
+
+        Iterator var2 = message.iterator();
+
+        while(var2.hasNext()) {
+            String s = (String)var2.next();
 
             if (player instanceof Player) {
                 if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
-                    player.sendMessage(color(PlaceholderAPI.setPlaceholders(((Player) player), message)));
+                    player.sendMessage(color(PlaceholderAPI.setPlaceholders(((Player) player), s)));
                 }
-            } else player.sendMessage(color(message));
-        }
-
-        public static void sendMessage(CommandSender player, List<String> message) {
-
-            Iterator var2 = message.iterator();
-
-            while(var2.hasNext()) {
-                String s = (String)var2.next();
-
-                if (player instanceof Player) {
-                    if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
-                        player.sendMessage(color(PlaceholderAPI.setPlaceholders(((Player) player), s)));
-                    }
-                } else player.sendMessage(color(s));
-            }
-
-        }
-
-
-        // get block face in target block
-        public static BlockFace getBlockFace(Player player) {
-            List<Block> lastTwoTargetBlocks = player.getLastTwoTargetBlocks(null, 100);
-            return lastTwoTargetBlocks.get(1).getFace(lastTwoTargetBlocks.get(0));
-        }
-
-
-        // Convert String to LOCATION
-        public static Location parseStringImpl(String impl) {
-            String[] components = impl.split(",");
-            World world = Bukkit.getServer().getWorld(components[0]);
-            int x = 0;
-            int y = 0;
-            int z = 0;
-
-            try {
-                x = Integer.parseInt(components[1]);
-                y = Integer.parseInt(components[2]);
-                z = Integer.parseInt(components[3]);
-            } catch (Exception var7) {
-                Exception err = var7;
-                err.printStackTrace();
-            }
-
-            Location loc = new Location(world, (double)x, (double)y, (double)z);
-            loc.setDirection(new Vector(Double.valueOf(components[4]), Double.valueOf(components[5]), Double.valueOf(components[6])));
-            return loc;
-        }
-
-        // Convert LOCATION TO STRING
-
-        public static String converttoString(Location location) {
-            String worldName = location.getWorld().getName();
-            String X = String.valueOf(location.getBlockX());
-            String Y = String.valueOf(location.getBlockY());
-            String Z = String.valueOf(location.getBlockZ());
-            return "{world},{x},{y},{z},{direction}".replace("{world}", worldName).replace("{x}", X).replace("{y}", Y).replace("{z}", Z).replace("{direction}", location.getDirection().toString());
-        }
-
-
-        public static void dispatchCommand(CommandSender sender, String command) {
-            if (command.contains("[console]")) {
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replace("[console] ", ""));
-                return;
-            }
-            if (command.contains("[player]")) {
-                Player player = (Player)sender;
-                Bukkit.dispatchCommand(sender, command.replace("[player] ", "").replace("%player%", player.getName()));
-                return;
-            }
-            if (command.contains("[message]")) {
-                LKUtils.sendMessage(sender, command.replace("[message] ", ""));
-            }
-        }
-
-        public static List<Integer> convertStringtoList(String a) {
-
-            String stripped = a.replaceAll("\\[|\\]", "");
-
-            String[] tokens = stripped.split(",");
-            List<Integer> integerList = new ArrayList<>();
-
-
-            for (String token : tokens) {
-                integerList.add(Integer.parseInt(token.trim()));
-            }
-
-            return integerList;
-        }
-
-
-        public static boolean comparewithString(Integer number1, Integer number2, String operator) {
-
-            switch (operator) {
-                case ">=":
-                    return number1 >= number2;
-                case ">":
-                    return number1 > number2;
-                case "<":
-                    return number1 < number2;
-                case "<=":
-                    return number1 <= number2;
-                case "==":
-                    return number1 == number2;
-                case "!=":
-                    return number1 != number2;
-                default:
-                    throw new IllegalArgumentException("Operator '" + operator + "' unvaliable!");
-            }
-
-        }
-
-        public static boolean removeItem(Player player, ItemStack item) {
-            Inventory inventory = player.getInventory();
-            int amountToRemove = item.getAmount();
-            for (int i = 0; i < inventory.getSize(); i++) {
-                ItemStack currentItem = inventory.getItem(i);
-
-                if (currentItem != null && currentItem.isSimilar(item)) {
-                    int currentAmount = currentItem.getAmount();
-
-                    if (currentAmount >= amountToRemove) {
-                        currentItem.setAmount(currentAmount - amountToRemove);
-                        inventory.setItem(i, currentItem);
-                        return true;
-                    } else {
-                        amountToRemove -= currentAmount;
-                        inventory.clear(i);
-                    }
-                }
-            }
-
-            return false;
+            } else player.sendMessage(color(s));
         }
 
     }
+
+
+    // get block face in target block
+    public static BlockFace getBlockFace(Player player) {
+        List<Block> lastTwoTargetBlocks = player.getLastTwoTargetBlocks(null, 100);
+        return lastTwoTargetBlocks.get(1).getFace(lastTwoTargetBlocks.get(0));
+    }
+
+
+    // Convert String to LOCATION
+    public static Location parseStringImpl(String impl) {
+        String[] components = impl.split(",");
+        World world = Bukkit.getServer().getWorld(components[0]);
+        int x = 0;
+        int y = 0;
+        int z = 0;
+
+        try {
+            x = Integer.parseInt(components[1]);
+            y = Integer.parseInt(components[2]);
+            z = Integer.parseInt(components[3]);
+        } catch (Exception var7) {
+            Exception err = var7;
+            err.printStackTrace();
+        }
+
+        Location loc = new Location(world, (double)x, (double)y, (double)z);
+        loc.setDirection(new Vector(Double.valueOf(components[4]), Double.valueOf(components[5]), Double.valueOf(components[6])));
+        return loc;
+    }
+
+    // Convert LOCATION TO STRING
+
+    public static String converttoString(Location location) {
+        String worldName = location.getWorld().getName();
+        String X = String.valueOf(location.getBlockX());
+        String Y = String.valueOf(location.getBlockY());
+        String Z = String.valueOf(location.getBlockZ());
+        return "{world},{x},{y},{z},{direction}".replace("{world}", worldName).replace("{x}", X).replace("{y}", Y).replace("{z}", Z).replace("{direction}", location.getDirection().toString());
+    }
+
+
+    public static void dispatchCommand(CommandSender sender, String command) {
+        if (command.contains("[console]")) {
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replace("[console] ", ""));
+            return;
+        }
+        if (command.contains("[player]")) {
+            Player player = (Player)sender;
+            Bukkit.dispatchCommand(sender, command.replace("[player] ", "").replace("%player%", player.getName()));
+            return;
+        }
+        if (command.contains("[message]")) {
+            LKUtils.sendMessage(sender, command.replace("[message] ", ""));
+        }
+    }
+
+    public static List<Integer> convertStringtoList(String a) {
+
+        String stripped = a.replaceAll("\\[|\\]", "");
+
+        String[] tokens = stripped.split(",");
+        List<Integer> integerList = new ArrayList<>();
+
+
+        for (String token : tokens) {
+            integerList.add(Integer.parseInt(token.trim()));
+        }
+
+        return integerList;
+    }
+
+
+    public static boolean comparewithString(Integer number1, Integer number2, String operator) {
+
+        switch (operator) {
+            case ">=":
+                return number1 >= number2;
+            case ">":
+                return number1 > number2;
+            case "<":
+                return number1 < number2;
+            case "<=":
+                return number1 <= number2;
+            case "==":
+                return number1 == number2;
+            case "!=":
+                return number1 != number2;
+            default:
+                throw new IllegalArgumentException("Operator '" + operator + "' unvaliable!");
+        }
+
+    }
+
+    public static boolean removeItem(Player player, ItemStack item) {
+        Inventory inventory = player.getInventory();
+        int amountToRemove = item.getAmount();
+        for (int i = 0; i < inventory.getSize(); i++) {
+            ItemStack currentItem = inventory.getItem(i);
+
+            if (currentItem != null && currentItem.isSimilar(item)) {
+                int currentAmount = currentItem.getAmount();
+
+                if (currentAmount >= amountToRemove) {
+                    currentItem.setAmount(currentAmount - amountToRemove);
+                    inventory.setItem(i, currentItem);
+                    return true;
+                } else {
+                    amountToRemove -= currentAmount;
+                    inventory.clear(i);
+                }
+            }
+        }
+
+        return false;
+    }
+
+}
